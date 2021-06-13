b434dda18dffb8ac98acc7549e04a9691e26b8d4
Shubham <shubhamshingla98@gmail.com>
2021-06-13 22:43:21 +0530

    BST

--- src/tree/bst/Q10.java
+++ src/tree/bst/Q10.java
@@ -1,7 +1,85 @@
 package tree.bst;
+import java.util.*;
+
 /*
  * kth largest
  */
 public class Q10 {
 
+	int count=1;
+	
+	// iterative (inorder) O(K)
+	 public int kthLargest(Node root,int K)
+	    {
+	        //Your code here
+	        Stack<Node> nodes = new Stack<>();
+	        Stack<Integer> status = new Stack<>();
+	        
+	        if(root!=null){
+	            nodes.push(root);
+	            status.push(0);
+	            
+	            int count = 1;
+	            
+	            while(!nodes.isEmpty()){
+	                
+	                int st = status.pop()+1;
+	                status.push(st);
+	                
+	                if(st == 1){
+	                    
+	                    if(nodes.peek().right!=null){
+	                        nodes.push(nodes.peek().right);
+	                        status.push(0);
+	                    }
+	                        
+	                }
+	                else if(st == 2){
+	                    if(count == K){
+	                        return nodes.pop().data;
+	                    }
+	                    else
+	                        count++;
+	                        
+	                    if(nodes.peek().left!=null){
+	                        nodes.push(nodes.peek().left);
+	                        status.push(0);
+	                    }
+	                        
+	                }
+	                else{
+	                    
+	                    nodes.pop();
+	                    status.pop();
+	                    
+	                }
+	            }
+	        }
+	        
+	        return -1;
+	        
+	    }
+
+	 //recursive O(K)
+	 public int kthLargest1(Node root,int K)
+	    {
+	        //Your code here
+	        if(root!=null){
+	            int right = kthLargest1(root.right, K);
+	            if(right > -1)
+	                return right;
+	                
+	            if(K == count)
+	                return root.data;
+	            else
+	                count++;
+	                
+	            return kthLargest1(root.left, K);
+	        }
+	        
+	        return -1;
+	        
+	    }
+
+	 //for O(h) same as Q9
 }
--- src/tree/bst/Q11.java
+++ src/tree/bst/Q11.java
@@ -1,7 +1,61 @@
 package tree.bst;
+import java.util.*;
 /*
  * bt to bst by maitaining same structure
  */
 public class Q11 {
 
+	public static TreeNode convertBtToBst(TreeNode root) {
+		
+		if(root!=null) {
+		
+			ArrayList<Integer> list = new ArrayList<>();
+			
+			list = traverse(root);
+			
+			Collections.sort(list);
+			
+			root = convert(root, list, new int[] {0});
+		}
+		
+		return root;
+	}
+	
+	
+	static TreeNode convert(TreeNode root, ArrayList<Integer> list, int[] index) {
+		
+		if(root!=null) {
+			root.left = convert(root.left, list, index);
+			root.data = list.get(index[0]);
+			index[0]++;
+			root.right = convert(root.right, list, index);
+		}
+		
+		return root;
+	}
+	
+	static ArrayList<Integer> traverse(TreeNode root){
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		if(root!=null) {
+		
+			LinkedList<TreeNode> queue = new LinkedList<>();
+			queue.offer(root);
+			
+			while(!queue.isEmpty()) {
+				
+				TreeNode node = queue.poll();
+				list.add(node.data);
+				
+				if(node.left!=null)
+					queue.offer(node.left);
+				
+				if(node.right!=null)
+					queue.offer(node.right);
+			}
+		}
+		
+		return list;
+	}
 }
