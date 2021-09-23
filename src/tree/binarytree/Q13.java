9947d3c18df177dd6c5a3bc37a5b331977fac82f
Shubham Singla <65089841+codingwithshubham@users.noreply.github.com>
2021-09-23 22:03:34 +0530

    Update Q13.java

--- src/tree/binarytree/Q13.java
+++ src/tree/binarytree/Q13.java
@@ -18,5 +18,46 @@ public class Q13 {
 	        
 	        
 	    }
+	
+	//iterative
+	public static boolean isMirror2(TreeNode root1, TreeNode root2){
+        
+        LinkedList<TreeNode> queue1 = new LinkedList<>();
+        LinkedList<TreeNode> queue2 = new LinkedList<>();
+        
+        queue1.offer(root1);
+        queue2.offer(root2);
+        
+        
+        while(!queue1.isEmpty() && !queue2.isEmpty()){
+            TreeNode n1 = queue1.poll();
+            TreeNode n2 = queue2.poll();
+            
+            if(n1 == null || n2==null){
+                if(n1!=n2)
+                    return false;
+            }
+            else{
+                if(n1.val!=n2.val)
+                    return false;
+                
+                queue1.offer(n1.left);
+                queue1.offer(n1.right);
+                
+                queue2.offer(n2.right);
+                queue2.offer(n2.left);
+                
+                
+            }
+                
+        }
+        
+        if(!queue1.isEmpty() || !queue2.isEmpty())
+            return false;
+        
+        return true;
+        
+        
+    }
 
 }
